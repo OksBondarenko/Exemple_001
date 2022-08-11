f1b25613816d5dbd4755b005b7ed152eda5d1824
Oksana bondarenko <roksanapopova@gmail.com>
2022-08-11 11:00:53 +0200

    Examples 008 dog and friend

---
+++ Examples008_dog/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+int distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+while(distance>10)
+{
+    if (friend == 1)
+    {
+        time = distance / (FirstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (SecondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine($"Количество раз, которое собака пробежит, равно - {count}");
